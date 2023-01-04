27802ee4f257195773943ec2123a5a5b8017eeb9
thetitorenko <ntitorenko@gmail.com>
2023-01-04 10:12:31 +0600

    gddl: some edit

--- gdrive_direct_link/gddl.py
+++ gdrive_direct_link/gddl.py
@@ -6,6 +6,15 @@ from time import sleep
 link = input('Paste the link -> ')
 url_sample = 'https://drive.google.com/uc?export=download&id='
 
+
+def get_close_output():
+    sec = 5
+    for _ in range(sec):
+        print(f'The script will close after {sec} sec ...')
+        sec -= 1
+        sleep(1)
+
+
 try:
     direct_link = url_sample + [i for i in link.split('/') if len(i) > 17][0]
 
@@ -13,14 +22,10 @@ try:
     if response.status_code == 200:
         pyperclip.copy(direct_link)
         print('The direct link was copied to the clipboard')
-        sec = 5
-        for _ in range(sec):
-            print(f'The script will close after {sec} sec ...')
-            sec -= 1
-            sleep(1)
+        get_close_output()
     else:
         print('Something went wrong. Please try again.')
-        sleep(5)
+        get_close_output()
 
 except Exception:
     print('It does not look like Google link')
