5158551b3b6d39e9221bf7f501e52852a69afa43
thetitorenko <ntitorenko@gmail.com>
2023-08-12 15:28:17 +0600

    edit sf

--- sf/sf_clean_page.py
+++ sf/sf_clean_page.py
@@ -1,8 +1,10 @@
 """ Clean SF web pages script v0.2 """
 
-from bs4 import BeautifulSoup
 import os, re
-# import 
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from html.entities import name2codepoint
 
 
 # Function to get a list of all HTML files in a given directory
@@ -21,15 +23,22 @@ def get_html_files(directory):
 script_dir = os.path.dirname(os.path.abspath(__file__))
 html_files = get_html_files(script_dir)
 
-# Stage 1 - Cleaning files
+# Stage 1: Cleaning web pages
 # Looping through files, cleaning and save them
 for html_file in html_files:
     # Open the HTML file
     with open(html_file, 'r', encoding='utf-8') as f:
-        html = f.read()
+        html_ = f.read()
 
     # Remove the SingleFile's comment
-    cleaned_html = re.sub(r'<!--.*?-->', '', html, flags=re.DOTALL)
+    cleaned_html = re.sub(r'<!--.*?-->', '', html_, flags=re.DOTALL)
+
+    # def unescape(s):
+    #     for name in name2codepoint:
+    #         s = s.replace(f'&{name};', chr(name2codepoint[name]))
+    #     return s
+
+    # corrected_html = unescape(str(cleaned_html))
 
     # Parse the HTML file
     soup = BeautifulSoup(cleaned_html, 'html.parser')
@@ -48,7 +57,7 @@ for html_file in html_files:
         'sf-sequence-tab-view__nav-buttons',                # Navigation buttons
         'sf-exam-timer sf-exam-wrapper-timer',              # Exam timer
         ]
-    
+
     # Loop through the classes and remove the tags
     for class_name in classes_to_remove:
         tags_to_remove = soup.find_all(class_=class_name)
@@ -59,35 +68,46 @@ for html_file in html_files:
     with open(html_file, 'w', encoding='utf-8') as f:
         f.write(str(soup))
 
-# # # Stage 2 - Cleaning answers
+# Stage 2: Cleaning answers
+# Looping through files
+for html_file in html_files:
+    # Create a new objects to start the ChromeDriver service
+    service = Service()
+    options = webdriver.FirefoxOptions()
+    options.set_preference('javascript.enabled', False)
+    driver = webdriver.Firefox(service=service, options=options)
 
-# # Looping through files, cleaning and save them
-# for html_file in html_files:
-#     # Open the HTML file
-#     with open(html_file, 'r') as f:
-#         html = f.read()
+    # Load the web page
+    driver.get(f'file:///{html_file}')
 
-#     # Parse the HTML file
-#     soup = BeautifulSoup(html, 'html.parser')
+    # Get the HTML content
+    html_ = driver.page_source
 
-#     # List of classes to remove
-#     classes_to_remove = [
-#         'status-icon',              # Answer status (check mark) 1
-#         'indicator-container',      # Answer status (check mark) 2
-#         'feedback-hint-correct',    # Answer feedback 1
-#         'feedback-content',         # Answer feedback 2
-#         'submission-feedback',      # Answer attempt counter
-#         'notification success notification-submit',    # Answer mark counter 1
-#         'problem-progress',         # Answer mark counter 2
-#         ]
+    # Parse the HTML file
+    soup = BeautifulSoup(html_, 'html.parser')
+    
+    # List of classes to remove
+    classes_to_remove = [
+        'status-icon',              # Answer status (check mark) 1
+        'indicator-container',      # Answer status (check mark) 2
+        'feedback-hint-correct',    # Answer feedback 1
+        'feedback-content',         # Answer feedback 2
+        'submission-feedback',      # Answer attempt counter
+        'notification success notification-submit',    # Answer mark counter 1
+        'problem-progress',         # Answer mark counter 2
+        ]
 
-#     # Loop through the classes and remove the tags
-#     for class_name in classes_to_remove:
-#         tags_to_remove = soup.find_all(class_=class_name)
-#         for tag in tags_to_remove:
-#             for parent in tag.find_parents():
-#                 parent.decompose()
+    # Iterate over all classes in classes_to_remove
+    for class_name in classes_to_remove:
+        # Use CSS selector to find elements with the class
+        for element in soup.select(f'.{class_name}'):
+            print(element)
+            # Decompose (delete) the element
+            element.decompose()
 
-#     # Save the modified HTML file
-#     with open(html_file.replace('.html', '_answer.html'), 'w') as f:
-#         f.write(str(soup))
+    # Write the modified HTML to a new file
+    with open(html_file, 'w', encoding='utf-8') as f:
+        f.write(str(soup))
+
+    # Close the Selenium driver
+    driver.quit()
